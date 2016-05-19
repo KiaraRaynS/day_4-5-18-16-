f92d245db4de74ed9f9ebc5e10e37c074a798b7c
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-19 14:28:19 -0400

    Word generator now works.

---
+++ word_generator.py
@@ -0,0 +1,18 @@
+import string
+import random
+import re
+
+
+def generate_random_word():
+    with open("/usr/share/dict/words") as opened_file:
+        list_of_words = opened_file.read()
+
+    list_of_words = list_of_words.lower()
+    for x in string.punctuation:
+        list_of_words = list_of_words.replace(x, '')
+
+    words_list = (list_of_words).split()
+    print(words_list)
+    random_word = random.choice(words_list)
+
+generate_random_word()
