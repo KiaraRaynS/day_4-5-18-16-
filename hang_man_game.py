b8ee4550904672329e78cbe53ec99d95307a62f3
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-19 16:35:00 -0400

    Fixed more bugs

--- hang_man_game.py
+++ hang_man_game.py
@@ -1,5 +1,7 @@
 from word_generator import generate_random_word
 random_word = generate_random_word()
+print_number = len(random_word)
+random_word = random_word.split()
 
 
 def get_user_response():
@@ -8,31 +10,34 @@ def get_user_response():
 
 
 def hang_man_game():
-    correct_guesses = []
-    incorrect_guesses = []
-    begin_game_word = ("_ " * len(random_word))
+    begin_game_word = ("_ " * print_number)
     count = 0
-    print("_ " * len(random_word))
 
-    while count <= 7:
+    while count <= 8:
+        incorrect_guesses = []
+        correct_guesses = []
         print(begin_game_word)
-        get_user_response()
-        remaining_guesses = "You have {} guesses remaining.".format(9 - count)
+        user_answer = get_user_response()
+        remaining_guesses = "You have {} guesses remaining.".format(7 - count)
+        print(incorrect_guesses)
+        print(remaining_guesses)
         if user_answer in random_word:
-            letter_location = random_word.index(letter)
-            begin_game_word.append(user_answer)[letter_location]
-            print(remaining_guesses)
+            for letter in random_word:
+                letter_index = random_word.index(user_answer)
+                begin_game_word[letter_index] = user_answer
             print("Correct guess!")
+            correct_guesses = correct_guesses.append(user_answer)
         elif user_answer in correct_guesses:
             print("You already guessed that")
-            print(remaining_guesses)
         elif user_answer in incorrect_guesses:
-            print(remaining_guesses)
             print("You already guessed that")
+        elif count == 7:
+            print("Sorry, you did not guess all the word letters.")
+            print(random_word)
+            break
         else:
             print("Sorry, that is incorrect")
-            print(remaining_guesses)
-            incorrect_guesses.append(user_answer)
+            incorrect_guesses = incorrect_guesses.append(user_answer)
             count += 1
 
 hang_man_game()
