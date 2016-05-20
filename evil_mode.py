bff93f93886506d56e20789e08d73f8fce142176
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-20 14:33:12 -0400

    WIP

--- evil_mode.py
+++ evil_mode.py
@@ -1,4 +1,9 @@
-import random
+from user_response import coin_flip
+from user_response import generate_word_length
+from user_response import get_user_response
+from user_response import begin_game
+
+
 with open("/usr/share/dict/words") as opened_file:
     list_of_words = opened_file.read()
 list_of_words = list_of_words.lower()
@@ -6,3 +11,53 @@ words_list = (list_of_words).split()
 
 
 def evil_mode():
+    word_length = generate_word_length()
+    for word in words_list:
+        possible_words = []
+        if len(word) == word_length:
+            possible_words = possible_words.append(word)
+            return possible_words
+
+    begin_word = list("_" * word_length)
+    begin_count = 0
+    correct_answers[]
+    incorrect_answers[]
+    while begin_count < 10:
+        print(begin_word)
+        response = get_user_response()
+        coin_flip()
+        if "_" not in begin_word:
+            print("Congradulations! You defeated evil mode.")
+        elif response in correct_answers:
+            print("You already guessed that.")
+        elif response in incorrect_answers:
+            print("You aready guessed that.")
+        elif coin_flip == True:
+            for word in words_list:
+                if response in word:
+                    del(word)
+                    return words_list
+            print("Sorry, incorrect guess.")
+            begin_count += 1
+            incorrect_answers = incorrect_ansers.append(response)
+            return incorrect_answers
+        else:
+            import operator
+            new_list = []
+            for word in words_list:
+                if response in word:
+                    new_list.append(word)
+            largest_list = {}
+            key = index
+            value = words
+            most_common_index = int(index)
+            for word in words_list:
+                if response[index] in largest_list:
+                    largest_list[index] +=1
+                else:
+                    largest_list[index] = 1
+                sorted_indexes = sorted(largest_list.items())
+            begin_word[index] = begin_word.append(response)
+
+
+begin_game()
