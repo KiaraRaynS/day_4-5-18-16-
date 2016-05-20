6c7ba531ebea0ad25cdc232a75aa0227e6a433a0
KiaraRaynS <sherman.rayn@outlook.com>
2016-05-20 19:24:11 -0400

    WIP

--- evil_mode.py
+++ evil_mode.py
@@ -18,6 +18,7 @@ def evil_mode():
         if len(word) == word_length:
             possible_words = possible_words.append(word)
             return possible_words
+    words_list = possible_words
 
     begin_word = list("_" * word_length)
     begin_count = 0
@@ -25,10 +26,15 @@ def evil_mode():
     incorrect_answers = []
     while begin_count < 10:
         print(begin_word)
+        print(correct_answers)
+        print(incorrect_answers)
         response = get_user_response()
         coin_flip()
         if "_" not in begin_word:
             print("Congradulations! You defeated evil mode.")
+        elif begin_count == 9:
+            print("Sorry, you failed to guess the word.")
+            print("Possible words left: {}".format(words_list))
         elif response in correct_answers:
             print("You already guessed that.")
         elif response in incorrect_answers:
@@ -44,11 +50,17 @@ def evil_mode():
             return incorrect_answers
         else:
             new_list = []
+            list_with_index = []
             for word in words_list:
                 if response in word:
                     new_list.append(word)
             response_index = greatest_index_list()
-
+            for word in new_list:
+                if response in word[response_index]:
+                    list_with_index.append(word)
+                    return list_with_index
+                words_list = list_with_index
+                begin_word = begin_word[response_index].append(response)
 
 
 begin_game()
